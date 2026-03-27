6fcc6243d5e9c9da224ce851741cc4438bf85cf4
poonam <poonambhagaur61@gmail.com>
2026-03-28 01:53:19 +0530

    first change in feature2

--- packet capture.py
+++ packet capture.py
@@ -9,3 +9,10 @@ import psutil
 
 data = psutil.net_io_counters()
 print(data.bytes_sent, data.bytes_recv)
+from scapy.all import sniff
+
+def packet_callback(packet):
+    if packet.haslayer("IP"):
+        print(packet["IP"].src, "->", packet["IP"].dst)
+
+sniff(prn=packet_callback, count=20)
